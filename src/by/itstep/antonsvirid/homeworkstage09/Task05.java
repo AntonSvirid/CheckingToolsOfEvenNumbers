c5f61b29c5acd605d703156677901915aaf567b5
AntonSvirid <adsvirid@tut.by>
2022-09-22 23:31:22 +0300

    Task05 modified

--- src/by/itstep/antonsvirid/homeworkstage09/Task05.java
+++ src/by/itstep/antonsvirid/homeworkstage09/Task05.java
@@ -3,6 +3,22 @@ package by.itstep.antonsvirid.homeworkstage09;
 import java.util.Scanner;
 
 public class Task05 {
+    public static String msg(double a, double b, double c) {
+        String msg = "Error, no one even number or more then one even number in input";
+
+        if (a % 2 == 0 && b % 2 != 0 && c % 2 != 0) {
+            msg = "Only number A is even";
+        } else if (a % 2 != 0 && b % 2 == 0 && c % 2 != 0) {
+            msg = "Only number B is even";
+        } else if (a % 2 != 0 && b % 2 != 0 && c % 2 == 0) {
+            msg = "Only number C is even";
+        }
+        return msg;
+    }
+    public static void print(String msg) {
+        System.out.println(msg);
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -13,8 +29,7 @@ public class Task05 {
         System.out.println("Please input number C: ");
         double c = scanner.nextDouble();
 
-        String msg = "Error, no one even number";
-
-
+        String msg = msg(a, b, c);
+        Task02.print(msg);
     }
 }
